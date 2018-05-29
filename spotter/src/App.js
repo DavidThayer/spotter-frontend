1d105e94a1b0609719bc09c1b8d57914c34e9c11
David Thayer <david.thayer@gmail.com>
2018-05-29 10:35:31 -0700

    react initialized

---
+++ spotter/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>Hello Spotter</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
